bdb650128ebe785fe7e8f456b464f30a6e4e90e3
cmartyr <cjoycemartyr@gmail.com>
2026-03-18 17:22:25 +1030

    feat: algorithm class can now print outputs for both release and debug

--- pathfinder.py
+++ pathfinder.py
@@ -5,26 +5,21 @@ from bfs import BFS
 
 mode = -1
 heuristic = -1
+search = None
 
 # Handle command line arguments
 if not (len(sys.argv) == 4 or len(sys.argv) == 5):
 	utils.exit_program("Incorrect number of arguments: python pathfinder.py [mode] [map] [algorithm] [heuristic]")
 
-match sys.argv[1]:
-	case "debug":
-		mode = 0
-	case "release":
-		mode = 1
-	case _:
-		utils.exit_program("Invalid mode: mode types are debug, release")
+if not(sys.argv[1] == "debug" or sys.argv[1] == "release"):
+	utils.exit_program("Invalid mode: mode types are debug, release")
 
 path_map = read_map(sys.argv[2])
-print(path_map.map_array)
 
 #algorithm
 match sys.argv[3]:
 	case "bfs":
-		bfs_search = BFS(path_map)
+		search = BFS(path_map)
 	case "ucs":
 		pass
 	case "astar":
@@ -39,3 +34,13 @@ match sys.argv[3]:
 				utils.exit_program("Invalid heuristic: heuristic types are euclidean, manhattan")
 	case _:
 		utils.exit_program("Invalid algorithm: algorithm types are bfs, ucs, astar.")
+
+search.search()
+
+match sys.argv[1]:
+	case "debug":
+		search.print_debug()
+	case "release":
+		search.print_release()
+	case _:
+		utils.exit_program("Invalid mode: mode types are debug, release")
--- utils.py
+++ utils.py
@@ -7,6 +7,13 @@ class PathMap:
 	end = (0, 0)
 	map_array =[[]]
 
+class Node:
+	def __init__(self, position, cost):
+		self.position = position
+		self.cost = cost
+		self.children = []
+		self.path = []
+
 class MapAlgorithm:
 	def __init__(self, path_map):
 		self.map_copy = path_map
@@ -14,7 +21,9 @@ class MapAlgorithm:
 		self.visits = self._MapAlgorithm__create_empty_map()
 		self.first_visit = self._MapAlgorithm__create_empty_map()
 		self.last_visit = self._MapAlgorithm__create_empty_map()
-		self.nodes = dict()
+		self.map_nodes = dict()
+		self.search_array = []
+		self.__create_nodes()
 	
 	def __create_empty_map(self):
 		new_map = []
@@ -27,6 +36,51 @@ class MapAlgorithm:
 					new_row.append("X")
 			new_map.append(new_row)
 		return(new_map)
+
+	def __create_nodes(self):
+		for i in range(self.map_copy.size[0]):
+			for j in range(self.map_copy.size[1]):
+				new_node = Node((i, j), self.map_copy.map_array[i][j])
+				self.map_nodes[(i, j)] = new_node
+
+	def search(self):
+		pass # will be overriden by inherited classes
+
+	def print_debug(self):
+		print("path:")
+		self.print_map(self.path)
+		print("#visits:")
+		self.print_map(self.visits)
+		print("first visit:")
+		self.print_map(self.first_visit)
+		print("last visit:")
+		self.print_map(self.last_visit)
+
+	def print_release(self):
+		self.print_map(self.path)
+
+	def print_map(self, map_arr):
+		max_digits = 0
+		for i in range(self.map_copy.size[0]):
+			for j in range(self.map_copy.size[1]):
+				digits = len(str(map_arr[i][j]))
+				if digits > max_digits:
+					max_digits = digits
+		
+		for m in range(self.map_copy.size[0]):
+			row_string = ""
+			for n in range(self.map_copy.size[1]):
+				num_string = ""
+				digits = len(str(map_arr[m][n]))
+				if digits < max_digits:
+					for space in range(max_digits - digits):
+						num_string += " "
+				num_string += str(map_arr[m][n])
+				row_string += num_string
+				if (n < self.map_copy.size[1] - 1):
+					row_string += " "
+			print(row_string)
+
 
 class MapInputError(Exception):
 	pass
